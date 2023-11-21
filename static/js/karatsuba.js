7245c464be7e65f07b93b95ad4f28d56d8cd516c
matheusfga <matheusrfga@gmail.com>
2023-11-20 21:07:17 -0300

    Added restriction in number size

--- static/js/karatsuba.js
+++ static/js/karatsuba.js
@@ -1,11 +1,15 @@
 export function visualizeKaratsuba() {
-    const num1 = prompt("Insira o primeiro número:");
+    const num1 = prompt("Insira o primeiro número (entre 4 e 8 dígitos):");
     const num2 = prompt("Insira o segundo número:");
 
     const num1Digits = num1.split('').map(Number);
     const num2Digits = num2.split('').map(Number);
     if (num1Digits.length != num2Digits.length) {
         alert("Números tem que ter a mesma quantidade de digitos!");
+        return 0;
+    }
+    if (num1Digits.length < 4 || num1Digits.length > 8) {
+        alert("Use números que tenham entre 4 e 8 dígitos!");
         return 0;
     }
 
@@ -40,30 +44,36 @@ function karatsubaMultiply(num1, num2) {
     }
 
     const half = Math.floor(n / 2);
-    const a = num1.slice(0, half);
-    const b = num1.slice(half);
-    const c = num2.slice(0, half);
-    const d = num2.slice(half);
+    const x1 = num1.slice(0, half);
+    const x0 = num1.slice(half);
+    const y1 = num2.slice(0, half);
+    const y2 = num2.slice(half);
+    // (x1.length < x0.length) ? x1.unshift(0) : (x1.length > x0.length) ? x0.unshift(0) : console.log('come on, now X1X0');
+    // (y1.length < y2.length) ? y1.unshift(0) : (y1.length > y2.length) ? y2.unshift(0) : console.log('come on, now Y1Y0');
+    // console.log('all before: ' + x1 + ' ' + x0 + ' ' + y1 + ' ' + y2);
 
-    const ac = karatsubaMultiply(a, c);
-    const bd = karatsubaMultiply(b, d);
-    // console.log('all: ' + a + ' ' + b + ' ' + c + ' ' + d);
-    // console.log('adds: ' + add(a, b) + ' ' + add(c, d));
-    const a_b = add(a, b);
-    const c_d = add(c, d);
-    (a_b.length < c_d.length) ? a_b.unshift(0) : (a_b.length > c_d.length) ? c_d.unshift(0) : 'come on, now'
-    const ad_bc = karatsubaMultiply(a_b, c_d);
+    const x1y1 = karatsubaMultiply(x1, y1);
+    const x0y0 = karatsubaMultiply(x0, y2);
+    // console.log('all: ' + x1 + ' ' + x0 + ' ' + y1 + ' ' + y2);
+    // console.log('adds: ' + add(x1, x0) + ' ' + add(y1, y2));
+    const x1_x0 = add(x1, x0);
+    const y1_y0 = add(y1, y2);
+    (x1_x0.length < y1_y0.length) ? x1_x0.unshift(0) : (x1_x0.length > y1_y0.length) ? y1_y0.unshift(0) : console.log('come on, now');
+    const z1 = karatsubaMultiply(x1_x0, y1_y0);
 
-    const step1 = ac.finalResult * Math.pow(10, n);
-    const step2 = (ad_bc.finalResult - ac.finalResult - bd.finalResult) * Math.pow(10, half);
-    const finalResult = step1 + step2 + bd.finalResult;
+    // console.log('ac(z2) = ' + x1y1.finalResult)
+    // console.log('ad_bc(z1) = ' + z1.finalResult)
+    // console.log('bd(z0) = ' + x0y0.finalResult)
+    const step1 = x1y1.finalResult * Math.pow(10, n);
+    const step2 = (z1.finalResult - x1y1.finalResult - x0y0.finalResult) * Math.pow(10, half);
+    const finalResult = step1 + step2 + x0y0.finalResult;
 
     return {
         steps: [
-            ...ac.steps,
-            ...bd.steps,
-            ...ad_bc.steps,
-            `Combine os resultados: ${step1} + ${step2} + ${bd.finalResult} = ${finalResult}`
+            ...x1y1.steps,
+            ...x0y0.steps,
+            ...z1.steps,
+            `Combine os resultados: ${step1} + ${step2} + ${x0y0.finalResult} = ${finalResult}`
         ],
         finalResult: finalResult
     };
@@ -81,9 +91,8 @@ function add(arr1, arr2) {
         carry = Math.floor(sum / 10);
     }
 
-    while (carry > 0) {
-        result.unshift(carry % 10);
-        carry = Math.floor(carry / 10);
+    if (carry > 0) {
+        result.unshift(1);
     }
 
     return result;
